0131dccf998c33dd86cb1f035e9993fed57c80b9
Sidhart Singh <isidhartsingh@gmail.com>
2023-06-18 21:32:20 +0530

    Build Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav">
+          <Navbar />
+        </GridItem>
+        <Show above="lg">
+          {/* above="lg" : Render on large screens and above */}
+          <GridItem area="aside" bg={"gold"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg={"dodgerblue"}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
