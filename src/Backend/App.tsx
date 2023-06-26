36bc92cd6093e1131e4d61583bc3a45730f2ffbb
Sidhart Singh <isidhartsingh@gmail.com>
2023-06-26 20:27:23 +0530

    Refactor for running 'npm run build'

--- src/Backend/App.tsx
+++ src/Backend/App.tsx
@@ -1,31 +1,36 @@
-/*
-import { useEffect, useState } from "react";
-import ProductList from "./Backend/components/ProductList";
+// App.tsx for ProductList
+// -----------------------
 
-// useEffect()
-function App() {
-  const [category, setCategory] = useState("");
+// import { useEffect, useState } from "react";
+// import ProductList from "./Backend/components/ProductList";
 
-  // Default Behavior : excecute after every render
-  // useEffect(() => {});
+// // useEffect()
+// function App() {
+//   const [category, setCategory] = useState("");
 
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => {
-          setCategory(event.target.value);
-          console.log(event.target.value);
-        }}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="HouseHold">HouseHold</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
-}
+//   // Default Behavior : excecute after every render
+//   // useEffect(() => {});
 
-export default App;
-*/
+//   return (
+//     <div>
+//       <select
+//         className="form-select"
+//         onChange={(event) => {
+//           setCategory(event.target.value);
+//           console.log(event.target.value);
+//         }}
+//       >
+//         <option value=""></option>
+//         <option value="Clothing">Clothing</option>
+//         <option value="HouseHold">HouseHold</option>
+//       </select>
+//       <ProductList category={category} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// 'App.tsx' cannot be compiled under '--isolatedModules' because it is considered a global script file.
+//  Add an import, export, or an empty 'export {}' statement to make it a module.
+export {};
