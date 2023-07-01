b1f7f59316b32c2a1cdbd8c80c2e8daeccb87637
Sidhart Singh <isidhartsingh@gmail.com>
2023-07-01 18:10:10 +0530

    Build Platform Selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,6 +4,7 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 import { Genre } from "../hooks/useGenres";
+import PlatformSelector from "./PlatformSelector";
 
 interface Props {
   selectedGenre: Genre | null;
@@ -26,6 +27,9 @@ const GameGrid = ({ selectedGenre }: Props) => {
 
         {/* Error */}
         {error && <Text>{error}</Text>}
+
+        {/* Platform Selector */}
+        <PlatformSelector />
 
         {/* GameGrid */}
         <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={3}>
