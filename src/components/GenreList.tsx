24f397dc2491d7e9eb79fd4b216f923bf01580bf
Sidhart Singh <isidhartsingh@gmail.com>
2023-07-02 16:01:32 +0530

    Refactor : Cleanup Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -21,31 +21,37 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
   const { data: genres, isLoading } = useGenres();
 
   return (
-    <List py={"2"}>
+    <>
       {isLoading && <Spinner />}
-      <Heading size={"lg"} py={"5"}>
+      <Heading size={"lg"} mt={"5"} mb={"2"}>
         Genres
       </Heading>
-      {genres.map((genre) => (
-        <ListItem key={genre.id} py={"6px"}>
-          <HStack>
-            <Image
-              src={getCroppedImageurl(genre.image_background)}
-              boxSize={"32px"}
-              borderRadius={"full"}
-            />
-            <Button
-              onClick={() => onSelectGenre(genre)}
-              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-              fontSize={"16px"}
-              variant={"link"}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+      <List py={"2"}>
+        {genres.map((genre) => (
+          <ListItem key={genre.id} py={"6px"}>
+            <HStack>
+              <Image
+                src={getCroppedImageurl(genre.image_background)}
+                boxSize={"32px"}
+                borderRadius={"full"}
+                // images will be scaled to fit the container while preserving the aspect ratio
+                objectFit={"cover"}
+              />
+              <Button
+                onClick={() => onSelectGenre(genre)}
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                fontSize={"16px"}
+                variant={"link"}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
