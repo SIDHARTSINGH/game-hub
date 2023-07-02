0b24fb0d738dbde26274a0d652a053c070310be6
Sidhart Singh <isidhartsingh@gmail.com>
2023-07-02 15:23:21 +0530

    Build search input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,22 @@
 import { Flex, HStack, Heading } from "@chakra-ui/react";
 
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./searchInput";
 
 const Navbar = () => {
   return (
     <>
-      <HStack justifyContent={"space-between"} py={"15px"} px={"20px"}>
-        <Flex>
-          <Heading
-            size="md"
-            fontWeight={"850"}
-            alignSelf="center"
-            px={5}
-            letterSpacing={10}
-          >
-            RAWG
-          </Heading>
-        </Flex>
+      <HStack py={"15px"} px={"20px"}>
+        <Heading
+          size="md"
+          fontWeight={"850"}
+          alignSelf="center"
+          px={5}
+          letterSpacing={10}
+        >
+          RAWG
+        </Heading>
+        <SearchInput />
         <ColorModeSwitch />
       </HStack>
     </>
